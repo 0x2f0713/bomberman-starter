f39d9eca0f5daa2923219e71ea41a8c38e9ebff1
Trần Duy Long <21020547@vnu.edu.vn>
2022-10-21 17:08:59 +0700

    Set Indefinite cycle play audio, add stop function

--- src/uet/oop/bomberman/audio/Music.java
+++ src/uet/oop/bomberman/audio/Music.java
@@ -1,4 +1,28 @@
 package uet.oop.bomberman.audio;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
 
 public class Music {
+
+    private Media media;
+    private MediaPlayer player;
+
+    public static Music stage_start = new Music("res/sounds/stage_start.mp3");
+    public static Music stage_theme = new Music("res/sounds/stage_theme.mp3");
+    public static Music title_screen = new Music("res/sounds/title_screen.mp3");
+    Music(String path) {
+        this.media = new Media(new File(path).toURI().toString());
+        this.player = new MediaPlayer(media);
+        this.player.setCycleCount(MediaPlayer.INDEFINITE);
+    }
+
+    public void play() {
+        this.player.play();
+    }
+    public void stop() {
+        this.player.stop();
+    }
 }
