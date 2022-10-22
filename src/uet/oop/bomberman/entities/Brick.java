c6742c3e7a5f5b929185f0a083f2cf4ae5af2c6f
Trần Duy Long <21020547@vnu.edu.vn>
2022-10-22 13:39:14 +0700

    Implement brick breaking

--- src/uet/oop/bomberman/entities/Brick.java
+++ src/uet/oop/bomberman/entities/Brick.java
@@ -1,16 +1,43 @@
 package uet.oop.bomberman.entities;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.graphics.Sprite;
 
 public class Brick extends Entity {
+    private boolean Break;
+    boolean Disappear;
+    private int timeAnimationChange;
 
     public Brick(int x, int y, Image img) {
         super(x, y, img);
+        Disappear = Break = false;
     }
 
     @Override
     public void update() {
+        if (Break) {
+            if (timeAnimationChange == 0) {
+                img = Sprite.brick_exploded.getFxImage();
+            } else if (timeAnimationChange == 2) {
+                img = Sprite.brick_exploded1.getFxImage();
+            } else if (timeAnimationChange == 4) {
+                img = Sprite.brick_exploded2.getFxImage();
+                Disappear = true;
+            }
+            timeAnimationChange++;
+        }
+    }
 
+    public boolean isDisappear() {
+        return Disappear;
+    }
+
+    public boolean isBreak() {
+        return Break;
+    }
+
+    public void setBreak(boolean aBreak) {
+        Break = aBreak;
     }
 
     @Override
