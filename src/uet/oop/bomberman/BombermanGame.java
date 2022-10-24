92f8990a612e1b1ac10df6553be49e92da9c6a71
0x2f0713 <namhaiha0308@gmail.com>
2022-10-24 21:36:58 +0700

    Add playing state switcher and game freezer

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -115,8 +115,10 @@ public class BombermanGame extends Application {
                 if (now - lastUpdate > 70000000) {
                     keyPressedListener();
                     keyReleasedListener();
-                    update();
-                    render();
+                    if (state.isPlaying) {
+                        update();
+                        render();
+                    }
                     lastUpdate = now;
                 }
             }
@@ -244,6 +246,10 @@ public class BombermanGame extends Application {
     private void keyPressedListener() {
         scene.setOnKeyPressed(event -> {
             switch (event.getCode()) {
+                case P:
+                    state.isPlaying = !state.isPlaying;
+                    hud.updateIsPausing(!state.isPlaying);
+                    break;
                 case UP:
                     goNorth = true;
                     break;
