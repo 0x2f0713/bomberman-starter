715828da5646513dbca0d5e5fdcc1de34d38a9a7
0x2f0713 <namhaiha0308@gmail.com>
2022-10-25 14:45:04 +0700

    declare game state management

---
+++ src/uet/oop/bomberman/GameState.java
@@ -0,0 +1,11 @@
+package uet.oop.bomberman;
+
+public class GameState {
+    public static int MAX_OPTION_NUMBER = 3;
+    public static int MAX_SOUND_NUMBER = 3;
+    public static int MAX_MUSIC_NUMBER = 3;
+
+    public int optionNumber = 0;
+    public int soundVolume = 10;
+    public int musicVolume = 10;
+}
