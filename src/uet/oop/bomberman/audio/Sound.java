e08cfa10d651e6392b6f0fa80e0570370fc0d4b2
0x2f0713 <namhaiha0308@gmail.com>
2022-10-30 16:40:15 +0700

    Use new sound technique

--- src/uet/oop/bomberman/audio/Sound.java
+++ src/uet/oop/bomberman/audio/Sound.java
@@ -1,4 +1,5 @@
 package uet.oop.bomberman.audio;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -7,10 +8,10 @@ import java.io.File;
 
 public class Sound {
 
-    private Media media;
-    private MediaPlayer player;
+    private AudioClip audioClip;
 
     public static Sound bomb_explored = new Sound("res/sounds/bomb_explored.wav");
+    public static Sound game_over = new Sound("res/sounds/game_over.mp3");
     public static Sound next_level = new Sound("res/sounds/next_level.wav");
     public static Sound placed_bomb = new Sound("res/sounds/placed_bomb.wav");
     public static Sound power_up = new Sound("res/sounds/power_up.wav");
@@ -21,15 +22,13 @@ public class Sound {
     public static Sound walk_4 = new Sound("res/sounds/walk_4.wav");
 
     Sound(String path) {
-        this.media = new Media(new File(path).toURI().toString());
-        this.player = new MediaPlayer(media);
-        this.player.setCycleCount(MediaPlayer.INDEFINITE);
+        this.audioClip = new AudioClip(new File(path).toURI().toString());
     }
 
     public void play() {
-        this.player.play();
+        this.audioClip.play();
     }
     public void stop() {
-        this.player.stop();
+        this.audioClip.stop();
     }
 }
